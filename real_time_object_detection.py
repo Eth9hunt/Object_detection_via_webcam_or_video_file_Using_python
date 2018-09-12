f7636d8f475b0b03237e6b5e444f4bd39a885a56
Resham_raj <43196652+Eth9hunt@users.noreply.github.com>
2018-09-12 14:20:06 +0530

    Add files via upload

---
+++ real_time_object_detection.py
@@ -0,0 +1,82 @@
+# In[ ]:
+
+import numpy as np
+import os
+
+import tensorflow as tf
+
+
+#%%
+
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
+#%%
+
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+#%%
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+#%%
+# Running the tensorflow session
+import cv2
+cap = cv2.VideoCapture('realvvv.mp4')
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    ret = True
+    while (ret):
+      ret,image_np = cap.read()
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      # Each box represents a part of the image where a particular object was detected.
+      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      # Each score represent how level of confidence for each of the objects.
+      # Score is shown on the result image, together with the class label.
+      scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      # Actual detection.
+      (boxes, scores, classes, num_detections) = sess.run(
+          [boxes, scores, classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})     
+    
+      
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),          
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+#     
+      cv2.imshow('image',cv2.resize(image_np,(860,740)))
+      if cv2.waitKey(15) & 0xFF == ord('q'):
+          break
+          cap.release()
+          cv2.destroyAllWindows()
+          
+    
